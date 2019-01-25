447590ec94a04d9b5eac89d3aee7cca095f55bdb
TevinThuku <tevinthuku@gmail.com>
2019-01-26 00:18:48 +0300

    Set up the user routes

--- app/api/v1/model/__init__.py
+++ app/api/v1/model/__init__.py
@@ -3,6 +3,7 @@
 from datetime import datetime
 
 QUESTIONS = []
+USERS = []
 
 
 class QuestionModel:
@@ -47,3 +48,46 @@ class QuestionModel:
         user get all questions that have ever been asked
         """
         return [QuestionModel.to_json(question) for question in QUESTIONS]
+
+
+class UserModel:
+    """
+    This is the user model class that contains our model setup
+    """
+
+    def __init__(self, firstname, username, lastname, email, password):
+        """
+        Start by defining each user attributes to use during the tests
+        Keep in mind the user is not an admin
+        """
+        self.user_id = len(USERS)+1
+        self.firstname = firstname
+        self.lastname = lastname
+        self.username = username
+        self.email = email
+        self.registered_on = datetime.now()
+        self.password = password
+        self.is_admin = False
+
+    # after sign-up save the user to the created dict , USERS_LEN
+    def save_user(self):
+        """
+        Add a new user to the users store
+        """
+        USERS.append(self)
+
+    # lets check the data store for any user
+    @staticmethod
+    def query_users(username, password):
+        return [UserModel.to_json(user) for user in USERS if user.username == username and user.password == password]
+
+    # return a json data , a readable dictionary object, including the date user was registered
+    @staticmethod
+    def to_json(user):
+        return {"firstname": user.firstname,
+                "lastname": user.lastname,
+                "username": user.username,
+                "email": user.email,
+                "password": user.password,
+
+                "registered_on": user.registered_on, }
